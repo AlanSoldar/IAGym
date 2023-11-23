a69081c0acd21ac038db651921b5bfa9f85f21d7
alansoldar <alan.soledar@hotmail.com>
2023-11-23 20:55:26 -0300

    m

--- main.py
+++ main.py
@@ -13,11 +13,31 @@ from tqdm import tqdm
 def main():
     env = gym.make('MountainCar-v0', render_mode='rgb_array')
     env = PreprocessEnv(env)
+    state_dims = env.observation_space.shape[0]
+    num_actions = env.action_space.n
     state = env.reset()
     action = torch.tensor(0)
     next_state, reward, done, _ = env.step(action)
     print(f"Sample state: {state}")
     print(f"Next state: {next_state}, Reward: {reward}, Done: {done}")
+
+    q_network = nn.Sequential(
+        nn.Linear(state_dims, 128),
+        nn.ReLU(),
+        nn.Linear(128, 64),
+        nn.ReLU(),
+        nn.Linear(64, num_actions)
+    )
+
+    target_q_network = copy.deepcopy(q_network)
+    target_q_network = target_q_network.eval()
+
+def policy(state, num_actions, q_network, epsilon=0.):    
+    if torch.rand(1) < epsilon:
+        return torch.randint(num_actions, (1, 1))
+    else:
+        av = q_network(state).detach()
+        return torch.argmax(av, dim=-1, keepdim=True)
 
 class PreprocessEnv(gym.Wrapper):
     def __init__(self, env):
